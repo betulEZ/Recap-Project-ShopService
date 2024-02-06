966149c46f698b8efcdf88a5fc22366ca78a0bdd
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-02-06 09:42:23 +0100

    main update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,8 @@ package org.example;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         List<Product> list=new ArrayList<>();
